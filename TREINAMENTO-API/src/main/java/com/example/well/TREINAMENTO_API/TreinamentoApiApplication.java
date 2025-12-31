4e9f6ae5daeae5d08c86e891e12a0a8a445b60b9
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2025-12-31 10:32:18 -0300

    PROJETO CRIADO

---
+++ TREINAMENTO-API/src/main/java/com/example/well/TREINAMENTO_API/TreinamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.well.TREINAMENTO_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoApiApplication.class, args);
+	}
+
+}
---
+++ TREINAMENTO-API/src/test/java/com/example/well/TREINAMENTO_API/TreinamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.well.TREINAMENTO_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
