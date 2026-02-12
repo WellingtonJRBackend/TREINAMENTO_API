427718ce6d26429cf237e3333be087b71e06f6ab
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2026-02-11 23:49:32 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- TREINAMENTO-API/src/main/java/com/example/well/TREINAMENTO_API/api/controller/CozinhaController.java
+++ TREINAMENTO-API/src/main/java/com/example/well/TREINAMENTO_API/api/controller/CozinhaController.java
@@ -70,4 +70,5 @@ public class CozinhaController {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
+
 }
--- TREINAMENTO-API/src/main/java/com/example/well/TREINAMENTO_API/domain/model/Restaurante.java
+++ TREINAMENTO-API/src/main/java/com/example/well/TREINAMENTO_API/domain/model/Restaurante.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
@@ -26,5 +28,11 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+    joinColumns = @JoinColumn(name = "restaurante_id"),
+    inverseJoinColumns = @JoinColumn (name = "forma_pagamento_id"))
+    private  List<FormaPagamento> formaPagamentos = new ArrayList<>();
 
 }
